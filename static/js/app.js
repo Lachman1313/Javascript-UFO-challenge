5ae62d32980fe95f08d429d77968ac54b4f5e5a9
Lachman1313 <52429015+Lachman1313@users.noreply.github.com>
2020-01-29 11:52:15 -0800

    finished up the button filter function

--- static/js/app.js
+++ static/js/app.js
@@ -17,3 +17,20 @@ data.forEach((dataRow) => {
     });
 })
 }
+// Button click to trigger an event
+function handleClick() {
+    d3.event.preventDefault();
+    let date = d3.select("#datetime").property("value");
+    let filterData = tableData;
+
+// Filter data using date entered to return rows where datetime value equals filter value
+    if (date) {
+        filterData = filterData.filter((row) => row.datetime === date);
+    }
+    
+    buildTable(filterData);
+}
+
+d3.selectAll("#filter-btn").on("click", handleClick);
+buildTable(tableData);
+buildTable(tableData);
